f75c5c51db1ff584d0a033c8c02f996ddc5a6f3f
Renzo Diaz <dev.renzo.diaz@gmail.com>
2025-07-30 13:56:56 -0500

    Add ActionText for richt text editor, and custom style for dashboard layout

--- app/models/job_history.rb
+++ app/models/job_history.rb
@@ -1,5 +1,6 @@
 class JobHistory < ApplicationRecord
   has_one_attached :cover
+  has_rich_text :description
 
   belongs_to :user
 end
---
+++ config/initializers/dartsass.rb
@@ -0,0 +1,4 @@
+Rails.application.config.dartsass.builds = {
+  "application.scss" => "application.css",
+  "dashboard.scss" => "dashboard.css"
+}
